361d8774af60ad1d88e7be518b20974dfbb85a9a
balloonbash <balloonbash.info@gmail.com>
2025-12-21 16:10:05 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
+async function inject(selector, url) {
+  const el = document.querySelector(selector);
+  if (!el) return;
+  const res = await fetch(url);
+  el.innerHTML = await res.text();
+}
 
+inject("#header", "/balloonbash/partials/header.html");
+inject("#footer", "/balloonbash/partials/footer.html");
